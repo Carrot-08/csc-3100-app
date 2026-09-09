8da4916b07e5e1ad9c1ef4b6b5d84a42475a4a38
Carrot-08 <angelpalacios2283@gmail.com>
2026-09-08 21:48:25 -0700

    IE2 1-4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,16 +4,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send(users);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
 const users = {
   users_list: [
     {
@@ -43,3 +33,28 @@ const users = {
     },
   ],
 };
+
+app.use(express.json());
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
